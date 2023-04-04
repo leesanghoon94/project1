db738c303f332b771e770b490ff65e72d454ffcf
Qook <anx2030@naver.com>
2023-04-04 23:11:39 +0900

    Makelectures/index.js수정

--- myproject/routes/Makelectures/index.js
+++ myproject/routes/Makelectures/index.js
@@ -4,9 +4,9 @@ module.exports = async function (fastify, opts) {
     fastify.post('/', async (req, reply) => {
         const client = await fastify.pg.connect()
         try {
-            const { lecname, day } = req.body
+            const { profid, lecname, day } = req.body
             const { rows } = await client.query(
-            'INSERT INTO public.lectures (lecname, day) VALUES ($1, $2) RETURNING *',[lecname, day]
+            'INSERT INTO public.lectures (profid,lecname, day) VALUES ($1, $2, $3) RETURNING *',[profid,lecname, day]
             )
             
             reply.code(201).send(rows)
