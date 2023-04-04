bd64fbaeac56487a5a424cb0225c49d703846ece
gahyunSong <gahyun8876@gmail.com>
2023-04-04 22:25:23 +0900

    modified students and lecture

--- myproject/routes/students/index.js
+++ myproject/routes/students/index.js
@@ -7,5 +7,18 @@ module.exports = async function (fastify, opts) {
         const { rows } = await client.query('SELECT * FROM students');
         client.release();
         return rows;
-        });
+        }),
+        fastify.put('/:stid', async (req, reply) => {
+            const client = await fastify.pg.connect()
+            try {
+                const { email } = req.body
+                const { rows } = await client.query(
+                    `UPDATE students SET email='${req.body.email}' WHERE email='${req.body.email}'`
+                )
+                
+                reply.code(201).send(rows)
+            } finally {
+                client.release()
+            }
+            })
 }
